77ddeab5dfed9cde72a7f408c0099278a1344216
Rachid Abu-Hassan <rachid@platform78.com>
2020-10-23 13:49:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,9 +17,9 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 
 // deleteFromObjectByKey(object, key)
 function deleteFromObjectByKey(object, key) {
-
-
-
+  var newObj = Object.assign({}, object)
+  delete newObj.key
+  return newObj
 }
 
 
