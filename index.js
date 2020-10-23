7e5a13d359c9221a26e4140f653b609be7dae598
Rachid Abu-Hassan <rachid@platform78.com>
2020-10-23 13:36:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ var recipes = {}
 
 // updateObjectWithKeyAndValue(object, key, value)
 function updateObjectWithKeyAndValue(object, key, value) {
-  
+  Object.assign()
 
 }
 
 // destructivelyUpdateObjectWithKeyAndValue(object, key, value)
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-
-
+  object[key] = value
+  return object
 }
 
 // deleteFromObjectByKey(object, key)
